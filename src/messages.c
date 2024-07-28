7555442336a37a951d7292d0f52a1d4a2c2454e0
Sousa <sousa.gates@gmail.com>
2024-07-28 20:41:55 -0300

    created some display messages

---
+++ src/messages.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void display_menu_message() { printf("Welcome to the Database\n"); }
+
+void display_ending_message() { printf("Finishing the program!\n"); }
+
+void display_help_message() { printf("TODO\n"); }
---
+++ src/messages.h
@@ -0,0 +1,8 @@
+// Displays menu messsage
+void display_menu_message();
+
+// Displays ending of the program message
+void display_ending_message();
+
+// Displays help message
+void display_help_message();
