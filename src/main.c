d044e1dd28e6f848d60ab2783441201e0d4ac094
Sousa <sousa.gates@gmail.com>
2024-07-28 20:42:17 -0300

    initial cli

--- src/main.c
+++ src/main.c
@@ -1,6 +1,31 @@
+#include "messages.h"
+#include "utils.h"
+#include <stdbool.h>
 #include <stdio.h>
-
+#include <string.h>
 int main() {
 
-    return 0;
+  char user_input[256];
+
+  display_menu_message();
+  while (true) {
+    fgets(user_input, 256, stdin);
+
+    if (user_input[0] == '\n') {
+      continue;
+    }
+
+    user_input[strlen(user_input) - 1] = '\0';
+
+    if (check_command(user_input, "help")) {
+      display_help_message();
+      continue;
+    } else if (check_command(user_input, "leave")) {
+      break;
+    }
+  }
+
+  display_ending_message();
+
+  return 0;
 }
