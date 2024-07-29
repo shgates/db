07a1c41c25c76d62fc6e52f2ed9910d8ea8f5b9d
Sousa <sousa.gates@gmail.com>
2024-07-29 10:56:52 -0300

    ädded an arrow for the user input

--- src/messages.c
+++ src/messages.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+void display_user_arrow() { printf("\n»» "); }
+
 void display_menu_message() { printf("Welcome to the Database\n"); }
 
 void display_ending_message() { printf("Finishing the program!\n"); }
 
-void display_help_message() { printf("TODO\n"); }
+void display_help_message() { printf("TO DO\n"); }
--- src/messages.h
+++ src/messages.h
@@ -1,3 +1,5 @@
+// Displays an arrow before the user input
+void display_user_arrow();
 // Displays menu messsage
 void display_menu_message();
 
